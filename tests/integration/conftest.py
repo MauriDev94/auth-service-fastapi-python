579c0d55bb41ac896fdec49436c8a20feab8b066
Mauricio Andres <mauri.nyc15@hotmail.com>
2025-11-20 17:00:38 -0300

    workflow for integration test

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,6 +1,6 @@
 import sys
 from pathlib import Path
 
-root_dir = Path(__file__).parent.parent
+root_dir = Path(__file__).parent.parent.parent
 print(f"Root directory: {root_dir}")
 sys.path.insert(0, str(root_dir))
